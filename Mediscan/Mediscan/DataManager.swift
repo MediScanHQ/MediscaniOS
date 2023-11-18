d86f410badb598c0ce3a57dbd494103a370d45ea
Alok Sahay <alok.sahay87@gmail.com>
2023-11-18 03:24:26 +0300

    Created endpoints for the data

---
+++ Mediscan/Mediscan/DataManager.swift
@@ -0,0 +1,24 @@
+//
+//  DataManager.swift
+//  Mediscan
+//
+//  Created by Alok Sahay on 18.11.2023.
+//
+
+import Foundation
+
+
+class DataManager {
+    
+    static let addRecordEndPoint = URL(string: "https://c40a-188-119-60-70.ngrok-free.app/api/new")
+    static let getRecordEndPoint = URL(string: "https://c40a-188-119-60-70.ngrok-free.app/api/find")
+    
+    func createNewRecord() {
+        
+    }
+    
+    func fetchRecord() {
+        
+    }
+    
+}
